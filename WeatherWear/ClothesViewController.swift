cc1b9ba75b265bbbe2c1a60f1e07ab075468f12a
cassied1028 <cassied1028@gmail.com>
2023-11-23 20:36:10 -0500

    added clothes button

---
+++ WeatherWear/ClothesViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ClothesViewController.swift
+//  WeatherWear
+//
+//  Created by Cassandra Davidson on 11/23/23.
+//
+
+import UIKit
+
+class ClothesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
--- WeatherWear/ViewController.swift
+++ WeatherWear/ViewController.swift
@@ -17,12 +17,23 @@ class ViewController: UIViewController {
         return button
     }()
     
+    private let clothesButton : UIButton = {
+        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 200, height: 52))
+        button.setTitle("Clothes", for: .normal)
+        button.backgroundColor = .purple
+        button.setTitleColor(.white, for: .normal)
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         view.backgroundColor = .systemBlue
         view.addSubview(alarmButton)
         alarmButton.addTarget(self, action: #selector(alarmButtonTap), for: .touchUpInside)
+        
+        view.addSubview(clothesButton)
+        clothesButton.addTarget(self, action: #selector(clothesButtonTap), for: .touchUpInside)
     }
    
     @objc func alarmButtonTap() {
@@ -41,9 +52,27 @@ class ViewController: UIViewController {
 
     }
     
+    @objc func clothesButtonTap() {
+        let tabBarVC = UITabBarController()
+        
+        let avc = UINavigationController(rootViewController: AlarmViewController())
+        
+        avc.title = "Clothes"
+        
+        tabBarVC.setViewControllers([avc], animated: false)
+        tabBarVC.modalPresentationStyle = .fullScreen
+        tabBarVC.tabBar.isTranslucent = false
+        tabBarVC.tabBar.backgroundColor = .white
+        tabBarVC.tabBar.tintColor = .blue
+        present(tabBarVC, animated: true)
+
+    }
+    
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
-        alarmButton.center = view.center
+        alarmButton.frame = CGRect(x: view.center.x - 100, y: view.center.y - 78, width: 200, height: 52)
+        
+        clothesButton.frame = CGRect(x: view.center.x - 100, y: alarmButton.frame.origin.y + alarmButton.frame.size.height + 10, width: 200, height: 52)
     }
 }
 
