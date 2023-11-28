59f77e8478928c130f1def33a8b980a5ab7681a8
zchown <91749140+zchown@users.noreply.github.com>
2023-11-28 13:01:19 -0500

    Alarm selector wheel and table view

---
+++ WeatherWear/AlarmListItem/AlarmListItem+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  AlarmListItem+CoreDataClass.swift
+//  WeatherWear
+//
+//  Created by Zander Chown on 11/16/23.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(AlarmListItem)
+public class AlarmListItem: NSManagedObject {
+
+}
---
+++ WeatherWear/AlarmListItem/AlarmListItem+CoreDataProperties.swift
@@ -0,0 +1,26 @@
+//
+//  AlarmListItem+CoreDataProperties.swift
+//  WeatherWear
+//
+//  Created by Zander Chown on 11/16/23.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension AlarmListItem {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<AlarmListItem> {
+        return NSFetchRequest<AlarmListItem>(entityName: "AlarmListItem")
+    }
+
+    @NSManaged public var dateTime: Date?
+    @NSManaged public var enabled: Bool
+
+}
+
+extension AlarmListItem : Identifiable {
+
+}
