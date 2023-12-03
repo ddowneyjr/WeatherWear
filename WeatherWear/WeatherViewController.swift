4d66b572fa7c372627e9a7825f095716828e2ac3
Derrell Downey Jr <derrell.downey@gmail.com>
2023-12-03 17:24:31 -0500

    pray

--- WeatherWear/WeatherViewController.swift
+++ WeatherWear/WeatherViewController.swift
@@ -14,7 +14,7 @@ import CoreLocation
 // After weekly forecast is displayed add another api call to get the current temp using the other link
 // Then display this information in the header to get the proper current data
 
-class WeatherViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, CLLocationManagerDelegate {
+class WeatherViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     let table: UITableView = {
         let table = UITableView()
@@ -25,21 +25,22 @@ class WeatherViewController: UIViewController, UITableViewDelegate, UITableViewD
     var models = [WeatherData]()
 //    var weekly = [DayWeather]()
     
-    
-    var forecast: Root? = nil
-    var currentWeather: CurrentDataItem? = nil
-    var currentTemp: Double? = nil
+    var weatherSingleton: WeatherSingleton? = nil
     
     
-    let locationManager = CLLocationManager()
+    private var currentWeather: CurrentDataItem?
+    private var currentTemp: Double?
+    private var forecast: Root?
     
     
-    var currentLocation: CLLocation?
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // register 2 cells
+        
+        weatherSingleton = WeatherSingleton.getInstance()
         
         
         
@@ -54,93 +55,112 @@ class WeatherViewController: UIViewController, UITableViewDelegate, UITableViewD
         
     }
     
+
+    
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
-        setupLocation()
-    }
-    
-    
-    
-    // Location
-    func setupLocation() {
-        locationManager.delegate = self
-        locationManager.requestWhenInUseAuthorization()
-        locationManager.startUpdatingLocation()
-    }
-    
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        if !locations.isEmpty, currentLocation == nil {
-            currentLocation = locations.first
-            locationManager.stopUpdatingLocation()
-            requestWeatherForLocation()
-        }
-    }
-    
-    func requestWeatherForLocation() {
         
-        guard let currentLocation = currentLocation else {
-            return
-        }
         
-        let lon = currentLocation.coordinate.longitude
-        let lat = currentLocation.coordinate.latitude
         
-        print("\(lon) | \(lat)")
-        
-        let key = "4103885f8dc34c5b8374feaf9ba81ecf"
-        
-        let currenturl = "https://api.weatherbit.io/v2.0/current?lat=\(lat)&lon=\(lon)&key=\(key)&units=I"
-        let url = "https://api.weatherbit.io/v2.0/forecast/daily?lat=\(lat)&lon=\(lon)&key=\(key)&units=I"
-        print(url)
-        
-        let currentApiService = CurrentAPIService(url: URL(string: currenturl))
-        currentApiService.fetchData { (root) in
-            guard let root = root else {
-                print("Error fetching data")
-                return
-            }
-            
-            print(root.data[0].temp)
-            print("______")
-            
-            self.currentWeather = root.data[0]
-            self.currentTemp = self.currentWeather?.temp
-            print(self.currentTemp!)
-            print(self.currentWeather!)
-            
-            
-        }
-        
-        let apiService = APIService(url: URL(string: url))
-        apiService.fetchData { (root) in
-            guard let root = root else {
-                print("Error fetching data")
-                return
-            }
-            
-            print(root.data[0].temp)
-            
-            self.forecast = root
-            
+        weatherSingleton?.requestWeatherForLocation {
+            // Access the weather data inside the completion handler
+            self.currentWeather = self.weatherSingleton?.currentWeather
+            self.currentTemp = self.weatherSingleton?.currentTemp
+            self.forecast = self.weatherSingleton?.forecast
             self.models = self.forecast!.data
-            
-//            for day in self.forecast!.data {
-//                self.weekly.append(DayWeather(highTemp: day.high_temp, lowTemp: day.low_temp, date: day.datetime))
-//            }
-            
-            print(self.models)
-            
+                
+            // Update the UI on the main thread
             DispatchQueue.main.async {
                 self.table.reloadData()
-
                 self.table.tableHeaderView = self.createTableHeader()
             }
-            
         }
         
         
     }
+    
+    
+    
+//    // Location
+//    func setupLocation() {
+//        locationManager.delegate = self
+//        locationManager.requestWhenInUseAuthorization()
+//        locationManager.startUpdatingLocation()
+//    }
+//    
+//    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+//        if !locations.isEmpty, currentLocation == nil {
+//            currentLocation = locations.first
+//            locationManager.stopUpdatingLocation()
+//            requestWeatherForLocation()
+//        }
+//    }
+//    
+//    func requestWeatherForLocation() {
+//        
+//        guard let currentLocation = currentLocation else {
+//            return
+//        }
+//        
+//        let lon = currentLocation.coordinate.longitude
+//        let lat = currentLocation.coordinate.latitude
+//        
+//        print("\(lon) | \(lat)")
+//        
+//        let key = "4103885f8dc34c5b8374feaf9ba81ecf"
+//        
+//        let currenturl = "https://api.weatherbit.io/v2.0/current?lat=\(lat)&lon=\(lon)&key=\(key)&units=I"
+//        let url = "https://api.weatherbit.io/v2.0/forecast/daily?lat=\(lat)&lon=\(lon)&key=\(key)&units=I"
+//        print(url)
+//        
+//        let currentApiService = CurrentAPIService(url: URL(string: currenturl))
+//        currentApiService.fetchData { (root) in
+//            guard let root = root else {
+//                print("Error fetching data")
+//                return
+//            }
+//            
+//            print(root.data[0].temp)
+//            print("______")
+//            
+//            self.currentWeather = root.data[0]
+//            self.currentTemp = self.currentWeather?.temp
+//            print(self.currentTemp!)
+//            print(self.currentWeather!)
+//            
+//            
+//        }
+//        
+//        let apiService = APIService(url: URL(string: url))
+//        apiService.fetchData { (root) in
+//            guard let root = root else {
+//                print("Error fetching data")
+//                return
+//            }
+//            
+//            print(root.data[0].temp)
+//            
+//            self.forecast = root
+//            
+//            self.models = self.forecast!.data
+//            
+////            for day in self.forecast!.data {
+////                self.weekly.append(DayWeather(highTemp: day.high_temp, lowTemp: day.low_temp, date: day.datetime))
+////            }
+//            
+//            print(self.models)
+//            
+//            DispatchQueue.main.async {
+//                self.table.reloadData()
+//
+//                self.table.tableHeaderView = self.createTableHeader()
+//            }
+//            
+//        }
+//        
+//        
+//    }
     
     func createTableHeader() -> UIView {
         let headerVIew = UIView(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: view.frame.size.width))
@@ -215,58 +235,58 @@ class WeatherViewController: UIViewController, UITableViewDelegate, UITableViewD
         }
 
     
-struct APIService {
-    let url: URL?
-
-    func fetchData(completion: @escaping (Root?) -> Void) {
-        guard let url = url else {
-            print("Invalid URL")
-            return
-        }
-        
-        URLSession.shared.dataTask(with: url) { (data, response, error) in
-            guard let data = data else {
-                print("No data in response: \(error?.localizedDescription ?? "Unknown error")")
-                return
-            }
-            
-            do {
-                let root = try JSONDecoder().decode(Root.self, from: data)
-                completion(root)
-            } catch {
-                print("Error decoding JSON: \(error)")
-                completion(nil)
-            }
-        }.resume()
-    }
-}
-    
-
-struct CurrentAPIService {
-    let url: URL?
-
-    func fetchData(completion: @escaping (CurrentRoot?) -> Void) {
-        guard let url = url else {
-            print("Invalid URL")
-            return
-        }
-        
-        URLSession.shared.dataTask(with: url) { (data, response, error) in
-            guard let data = data else {
-                print("No data in response: \(error?.localizedDescription ?? "Unknown error")")
-                return
-            }
-            
-            do {
-                let root = try JSONDecoder().decode(CurrentRoot.self, from: data)
-                completion(root)
-            } catch {
-                print("Error decoding JSON: \(error)")
-                completion(nil)
-            }
-        }.resume()
-    }
-}
+//struct APIService {
+//    let url: URL?
+//
+//    func fetchData(completion: @escaping (Root?) -> Void) {
+//        guard let url = url else {
+//            print("Invalid URL")
+//            return
+//        }
+//        
+//        URLSession.shared.dataTask(with: url) { (data, response, error) in
+//            guard let data = data else {
+//                print("No data in response: \(error?.localizedDescription ?? "Unknown error")")
+//                return
+//            }
+//            
+//            do {
+//                let root = try JSONDecoder().decode(Root.self, from: data)
+//                completion(root)
+//            } catch {
+//                print("Error decoding JSON: \(error)")
+//                completion(nil)
+//            }
+//        }.resume()
+//    }
+//}
+//    
+//
+//struct CurrentAPIService {
+//    let url: URL?
+//
+//    func fetchData(completion: @escaping (CurrentRoot?) -> Void) {
+//        guard let url = url else {
+//            print("Invalid URL")
+//            return
+//        }
+//        
+//        URLSession.shared.dataTask(with: url) { (data, response, error) in
+//            guard let data = data else {
+//                print("No data in response: \(error?.localizedDescription ?? "Unknown error")")
+//                return
+//            }
+//            
+//            do {
+//                let root = try JSONDecoder().decode(CurrentRoot.self, from: data)
+//                completion(root)
+//            } catch {
+//                print("Error decoding JSON: \(error)")
+//                completion(nil)
+//            }
+//        }.resume()
+//    }
+//}
 
 }
 
@@ -280,114 +300,114 @@ struct CurrentAPIService {
 
 // Structs for the week forecast
 
-struct Root: Codable {
-    let city_name: String
-    let country_code: String
-    let data: [WeatherData]
-    let lat: Double
-    let lon: Double
-    let state_code: String
-    let timezone: String
-}
-
-struct WeatherData: Codable {
-    let app_max_temp: Double
-    let app_min_temp: Double
-    let clouds: Double
-    let clouds_hi: Double
-    let clouds_low: Double
-    let clouds_mid: Double
-    let datetime: String
-    let dewpt: Double
-    let high_temp: Double
-    let low_temp: Double
-    let max_dhi: Double?
-    let max_temp: Double
-    let min_temp: Double
-    let moon_phase: Double
-    let moon_phase_lunation: Double
-    let moonrise_ts: Double
-    let moonset_ts: Double
-    let ozone: Double
-    let pop: Double
-    let precip: Double
-    let pres: Double
-    let rh: Double
-    let slp: Double
-    let snow: Double
-    let snow_depth: Double
-    let sunrise_ts: Double
-    let sunset_ts: Double
-    let temp: Double
-    let ts: Double
-    let uv: Double
-    let valid_date: String
-    let vis: Double
-    let weather: Weather
-    let wind_cdir: String
-    let wind_cdir_full: String
-    let wind_dir: Double
-    let wind_gust_spd: Double
-    let wind_spd: Double
-}
-
-struct Weather: Codable {
-    let description: String
-    let code: Double
-    let icon: String
-}
-
-
-// Structs for the current day weather
-
-struct CurrentWeather: Codable {
-    let code: Int
-    let icon: String
-    let description: String
-}
-
-struct CurrentDataItem: Codable {
-    let app_temp: Double
-    let aqi: Double
-    let city_name: String
-    let clouds: Double
-    let country_code: String
-    let datetime: String
-    let dewpt: Double
-    let dhi: Double
-    let dni: Double
-    let elev_angle: Double
-    let ghi: Double
-    let gust: Double?
-    let h_angle: Double
-    let lat: Double
-    let lon: Double
-    let ob_time: String
-    let pod: String
-    let precip: Double
-    let pres: Double
-    let rh: Double
-    let slp: Double
-    let snow: Double
-    let solar_rad: Double
-    let sources: [String]
-    let state_code: String
-    let station: String
-    let sunrise: String
-    let sunset: String
-    let temp: Double
-    let timezone: String
-    let ts: Double
-    let uv: Double
-    let vis: Double
-    let weather: Weather
-    let wind_cdir: String
-    let wind_cdir_full: String
-    let wind_dir: Double
-    let wind_spd: Double
-}
-
-struct CurrentRoot: Codable {
-    let count: Int
-    let data: [CurrentDataItem]
-}
+//struct Root: Codable {
+//    let city_name: String
+//    let country_code: String
+//    let data: [WeatherData]
+//    let lat: Double
+//    let lon: Double
+//    let state_code: String
+//    let timezone: String
+//}
+//
+//struct WeatherData: Codable {
+//    let app_max_temp: Double
+//    let app_min_temp: Double
+//    let clouds: Double
+//    let clouds_hi: Double
+//    let clouds_low: Double
+//    let clouds_mid: Double
+//    let datetime: String
+//    let dewpt: Double
+//    let high_temp: Double
+//    let low_temp: Double
+//    let max_dhi: Double?
+//    let max_temp: Double
+//    let min_temp: Double
+//    let moon_phase: Double
+//    let moon_phase_lunation: Double
+//    let moonrise_ts: Double
+//    let moonset_ts: Double
+//    let ozone: Double
+//    let pop: Double
+//    let precip: Double
+//    let pres: Double
+//    let rh: Double
+//    let slp: Double
+//    let snow: Double
+//    let snow_depth: Double
+//    let sunrise_ts: Double
+//    let sunset_ts: Double
+//    let temp: Double
+//    let ts: Double
+//    let uv: Double
+//    let valid_date: String
+//    let vis: Double
+//    let weather: Weather
+//    let wind_cdir: String
+//    let wind_cdir_full: String
+//    let wind_dir: Double
+//    let wind_gust_spd: Double
+//    let wind_spd: Double
+//}
+//
+//struct Weather: Codable {
+//    let description: String
+//    let code: Double
+//    let icon: String
+//}
+//
+//
+//// Structs for the current day weather
+//
+//struct CurrentWeather: Codable {
+//    let code: Int
+//    let icon: String
+//    let description: String
+//}
+//
+//struct CurrentDataItem: Codable {
+//    let app_temp: Double
+//    let aqi: Double
+//    let city_name: String
+//    let clouds: Double
+//    let country_code: String
+//    let datetime: String
+//    let dewpt: Double
+//    let dhi: Double
+//    let dni: Double
+//    let elev_angle: Double
+//    let ghi: Double
+//    let gust: Double?
+//    let h_angle: Double
+//    let lat: Double
+//    let lon: Double
+//    let ob_time: String
+//    let pod: String
+//    let precip: Double
+//    let pres: Double
+//    let rh: Double
+//    let slp: Double
+//    let snow: Double
+//    let solar_rad: Double
+//    let sources: [String]
+//    let state_code: String
+//    let station: String
+//    let sunrise: String
+//    let sunset: String
+//    let temp: Double
+//    let timezone: String
+//    let ts: Double
+//    let uv: Double
+//    let vis: Double
+//    let weather: Weather
+//    let wind_cdir: String
+//    let wind_cdir_full: String
+//    let wind_dir: Double
+//    let wind_spd: Double
+//}
+//
+//struct CurrentRoot: Codable {
+//    let count: Int
+//    let data: [CurrentDataItem]
+//}
