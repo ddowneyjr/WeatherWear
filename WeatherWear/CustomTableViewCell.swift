1f101e4bfc863176a40b78d0e4f317db7cd5848b
cassied1028 <cassied1028@gmail.com>
2023-12-05 13:31:30 -0500

    smart clothes added

--- WeatherWear/CustomTableViewCell.swift
+++ WeatherWear/CustomTableViewCell.swift
@@ -2,8 +2,7 @@ import UIKit
 
 class CustomTableViewCell: UITableViewCell {
 
-    //WeatherSingleton
-    
+    var weatherSingleton: WeatherSingleton? = nil
     
     static let identifier = "CustomTableViewCell"
 
@@ -51,6 +50,9 @@ class CustomTableViewCell: UITableViewCell {
             UIImageView(image: UIImage(named: "warm jacket"))
         ]
     ]
+    
+//    categoryImages.removeAll()
+//    categoryImages = getWeather()
 
     func configure(category: String) {
         categoryText.text = category
@@ -81,6 +83,9 @@ class CustomTableViewCell: UITableViewCell {
         // Set content size of the scroll view
         //let totalWidth = contentView.frame.width * CGFloat(titleArray.count)
         //clothesScroll.contentSize = CGSize(width: totalWidth, height: contentView.frame.height)
+        categoryImages.removeAll()
+        categoryImages = getWeather()
+        
     }
 
     required init?(coder: NSCoder) {
@@ -89,11 +94,140 @@ class CustomTableViewCell: UITableViewCell {
 
     override func layoutSubviews() {
         super.layoutSubviews()
-
+        //getWeather()
         categoryText.frame = CGRect(x: 5, y: 5, width: contentView.frame.size.width - 50, height: 25)
         clothesScroll.frame = CGRect(x: 5, y: 40, width: contentView.frame.size.width - 10, height: 100)
         
         //============= adding this line separates the cells but adds a white border i cant figure out how to change =============
         //contentView.frame = contentView.frame.inset(by: UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10))
+    }
+    
+    func getWeather() -> [String: [UIImageView]] {
+        weatherSingleton = WeatherSingleton.getInstance()
+        let temp = weatherSingleton?.currentWeather?.temp ?? -999
+       
+        if (temp == -999){
+            print("Temperature not available")
+        }else{
+            print("Temperature: \(temp)")
+        }
+        
+        let rain = weatherSingleton?.currentWeather?.precip ?? -999
+        if (rain == -999){
+            print("Rain not available")
+        }else{
+            print("Rain: \(rain)")
+        }
+        
+        var imageDict: [String: [UIImageView]] = [:]
+        var tempClothes:[UIImageView] = []
+
+//***** might run into pointer issues *****
+        
+        //adding images for different temps
+        if(temp <= 30){
+            //hats
+            tempClothes.append(UIImageView(image: UIImage(named: "beanie")))
+            imageDict["Hats"] = tempClothes
+            tempClothes.removeAll()
+
+            //tops
+            tempClothes.append( UIImageView(image: UIImage(named: "longsleeve")))
+            imageDict["Tops"] = tempClothes
+            tempClothes.removeAll()
+            
+            //bottoms
+            tempClothes.append(UIImageView(image: UIImage(named: "pants")))
+            imageDict["Bottoms"] = tempClothes
+            tempClothes.removeAll()
+            
+            //footwear
+            tempClothes.append(UIImageView(image: UIImage(named: "snowboots")))
+            imageDict["Footwear"] = tempClothes
+            tempClothes.removeAll()
+            
+            //outerwear
+            tempClothes.append(UIImageView(image: UIImage(named: "sweater")))
+            tempClothes.append(UIImageView(image: UIImage(named: "warm jacket")))
+            imageDict["Outerwear"] = tempClothes
+            
+        }else if(temp > 30 && temp <= 65){
+            //hats
+            tempClothes.append(UIImageView(image: UIImage(named: "beanie")))
+            tempClothes.append(UIImageView(image: UIImage(named: "cap")))
+            imageDict["Hats"] = tempClothes
+            tempClothes.removeAll()
+            
+            //tops
+            tempClothes.append(UIImageView(image: UIImage(named: "longsleeve")))
+            if(temp >= 60){
+                tempClothes.append(UIImageView(image: UIImage(named: "shortsleeve")))
+                tempClothes.append(UIImageView(image: UIImage(named: "blouse")))
+            }
+            imageDict["Tops"] = tempClothes
+            tempClothes.removeAll()
+            
+            //bottoms
+            tempClothes.append(UIImageView(image: UIImage(named: "pants")))
+            if(temp >= 60){
+                tempClothes.append(UIImageView(image: UIImage(named: "shorts")))
+                tempClothes.append(UIImageView(image: UIImage(named: "skirt")))
+            }
+            imageDict["Bottoms"] = tempClothes
+            tempClothes.removeAll()
+            
+            //footwear
+            tempClothes.append(UIImageView(image: UIImage(named: "sneakers")))
+            tempClothes.append(UIImageView(image: UIImage(named: "snowboots")))
+            imageDict["Footwear"] = tempClothes
+            tempClothes.removeAll()
+            
+            //outerwear
+            tempClothes.append(UIImageView(image: UIImage(named: "sweater")))
+            tempClothes.append(UIImageView(image: UIImage(named: "warmjacket")))
+            tempClothes.append(UIImageView(image: UIImage(named: "lightjacket")))
+            imageDict["Outerwear"] = tempClothes
+            tempClothes.removeAll()
+            
+        }else{
+            //hats
+            tempClothes.append(UIImageView(image: UIImage(named: "cap")))
+            imageDict["Hats"] = tempClothes
+            tempClothes.removeAll()
+            
+            //tops
+            tempClothes.append(UIImageView(image: UIImage(named: "shorts")))
+            tempClothes.append(UIImageView(image: UIImage(named: "skirt")))
+            tempClothes.append(UIImageView(image: UIImage(named: "tanktop")))
+            imageDict["Tops"] = tempClothes
+            tempClothes.removeAll()
+            
+            //bottoms
+            tempClothes.append(UIImageView(image: UIImage(named: "shorts")))
+            tempClothes.append(UIImageView(image: UIImage(named: "skirt")))
+            imageDict["Bottoms"] = tempClothes
+            tempClothes.removeAll()
+            
+            //footwear
+            tempClothes.append(UIImageView(image: UIImage(named: "sandals")))
+            tempClothes.append(UIImageView(image: UIImage(named: "flipflops")))
+            tempClothes.append(UIImageView(image: UIImage(named: "sneakers")))
+            imageDict["Footwear"] = tempClothes
+            tempClothes.removeAll()
+            
+            //outerwear
+            tempClothes.append(UIImageView(image: UIImage(named: "vest")))
+            imageDict["Outerwear"] = tempClothes
+            tempClothes.removeAll()
+        }
+        
+        //adding images for different amount of rain
+        if(rain >= 1.0){
+            imageDict["Hats"]?.append(UIImageView(image: UIImage(named: "rainhat")))
+            imageDict["Outerwear"]?.append(UIImageView(image: UIImage(named: "raincoat")))
+        }
+        
+        //print(imageDict)
+        return imageDict
     }
 }
